12e508b14e34cf494d877e1583851b78fe8af03c
Park HJ <phj3137@naver.com>
2025-07-25 16:19:42 +0900

    feat: ribbon 문구 변경

--- src/app/slogan/components/RibbonAnimation.tsx
+++ src/app/slogan/components/RibbonAnimation.tsx
@@ -13,51 +13,59 @@ const RibbonAnimation: React.FC = () => {
         </defs>
         <path strokeWidth="200" strokeMiterlimit="0" fill="none" id="text-path" d="M-98 194C260 326 450 318 770 294 1010 262 1200 182 1430 150 1530 134 1631 129 1718 202.8 1810 289 1810 438 1540 510 1190 606 810 470 430 550 251 590 140 670 160 790 192 910 308 917 410 926 530 934 620 918 790 886 943 854 1100 806 1240 790 1500 750 1720 774 2120 945" />
         <text className="ribbon__text">
-          <textPath fill="black" xlinkHref="#text-path" startOffset="0" textAnchor="middle" alignmentBaseline="central">
-            <animate attributeName="startOffset" from="0%" to="-100%" begin="0s" dur="25s" repeatCount="indefinite"></animate>
-            Stargazing Opportunities —
-            Scenic Waterfalls —
-            Stargazing Opportunities —
-            360° Panoramic Views —
-            3 Campgrounds -
-            Stargazing Opportunities —
-            Scenic Waterfalls —
-            Stargazing Opportunities —
-            360° Panoramic Views —
-            3 Campgrounds -
-            Stargazing Opportunities —
-            Scenic Waterfalls —
-            Stargazing Opportunities —
-            360° Panoramic Views —
-            3 Campgrounds -
-            Stargazing Opportunities —
-            Scenic Waterfalls —
-            Stargazing Opportunities —
-            360° Panoramic Views —
-            3 Campgrounds
-          </textPath>
+        <textPath fill="black" xlinkHref="#text-path" startOffset="0" textAnchor="middle" alignmentBaseline="central">
+          <animate attributeName="startOffset" from="0%" to="-100%" begin="0s" dur="25s" repeatCount="indefinite" />
+          We Own the Details —
+          We Own the Process —
+          We Own the Outcome —
+          We Own Our Work —
+          We Own the Impact —
+          We Own the Conversation —
+          We Own the Standards —
+          We Own the Vision —
+          We Own the Roadblocks —
+          We Own the Execution —
+          We Own the Strategy —
+          We Own the Why —
+          We Own the Details —
+          We Own the Process —
+          We Own the Outcome —
+          We Own Our Work —
+          We Own the Impact —
+          We Own the Conversation —
+          We Own the Standards —
+          We Own the Vision —
+          We Own the Roadblocks —
+          We Own the Execution —
+          We Own the Strategy —
+          We Own the Why —
+        </textPath>
           <textPath xlinkHref="#text-path" startOffset="0" textAnchor="middle" alignmentBaseline="central">
-            <animate attributeName="startOffset" from="0%" to="-100%" begin="0s" dur="25s" repeatCount="indefinite"></animate>
-            Stargazing Opportunities —
-            Scenic Waterfalls —
-            Stargazing Opportunities —
-            360° Panoramic Views —
-            3 Campgrounds -
-            Stargazing Opportunities —
-            Scenic Waterfalls —
-            Stargazing Opportunities —
-            360° Panoramic Views —
-            3 Campgrounds -
-            Stargazing Opportunities —
-            Scenic Waterfalls —
-            Stargazing Opportunities —
-            360° Panoramic Views —
-            3 Campgrounds -
-            Stargazing Opportunities —
-            Scenic Waterfalls —
-            Stargazing Opportunities —
-            360° Panoramic Views —
-            3 Campgrounds
+            <animate attributeName="startOffset" from="-0.3%" to="-100.25%" begin="0s" dur="25s" repeatCount="indefinite"></animate>
+            We Own the Details —
+            We Own the Process —
+            We Own the Outcome —
+            We Own Our Work —
+            We Own the Impact —
+            We Own the Conversation —
+            We Own the Standards —
+            We Own the Vision —
+            We Own the Roadblocks —
+            We Own the Execution —
+            We Own the Strategy —
+            We Own the Why —
+            We Own the Details —
+            We Own the Process —
+            We Own the Outcome —
+            We Own Our Work —
+            We Own the Impact —
+            We Own the Conversation —
+            We Own the Standards —
+            We Own the Vision —
+            We Own the Roadblocks —
+            We Own the Execution —
+            We Own the Strategy —
+            We Own the Why —
           </textPath>
         </text>
       </svg>
